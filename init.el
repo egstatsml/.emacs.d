b4be67a77c9c696389fdff78db832af5f3e7f87b
ethan <ethanjgoan@gmail.com>
2019-03-26 15:40:39 +1000

    commented out matlab section

--- init.el
+++ init.el
@@ -240,14 +240,14 @@
 
 
 ;;settings for MATLAB
-(add-to-list 'load-path "~/.emacs.d/packages/matlab/matlab.el")
-(load-library "matlab-load")
-(add-hook 'matlab-mode-hook 'company-mode)
-(add-hook 'matlab-shell-mode-hook 'company-mode)
-(setq auto-mode-alist
-    (cons
-     '("\\.m$" . matlab-mode)
-     auto-mode-alist))
+;;(add-to-list 'load-path "~/.emacs.d/packages/matlab/matlab.el")
+;;(load-library "matlab-load")
+;;(add-hook 'matlab-mode-hook 'company-mode)
+;;(add-hook 'matlab-shell-mode-hook 'company-mode)
+;;(setq auto-mode-alist
+;;    (cons
+;;     '("\\.m$" . matlab-mode)
+;;     auto-mode-alist))
 
 
 ;;LaTeX
