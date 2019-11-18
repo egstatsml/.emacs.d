267cfe0d497af8c1f692ff7b0071c58dff8fa788
Ethan <ethanjgoan@gmail.com>
2019-11-18 15:59:48 +1000

    use flymake with pylint

--- init.el
+++ init.el
@@ -256,6 +256,25 @@ Code:
   (add-to-list 'auto-mode-alist '("\\.pxd\\'" . cython-mode))
   (add-to-list 'auto-mode-alist '("\\.pxi\\'" . cython-mode)))
 
+;; Configure to wait a bit longer after edits before starting
+(setq-default flymake-no-changes-timeout '3)
+(setq-default flycheck-pylintrc "~/.emacs.d/.pylintrc")
+;; To avoid having to mouse hover for the error message,
+;; these functions make flymake error messages appear in the minibuffer
+(defun show-fly-err-at-point ()
+  "If the cursor is sitting on a flymake error, display the message in the minibuffer"
+  (require 'cl)
+  (interactive)
+  (let ((line-no (line-number-at-pos)))
+    (dolist (elem flymake-err-info)
+      (if (eq (car elem) line-no)
+      (let ((err (car (second elem))))
+        (message "%s" (flymake-ler-text err)))))))
+
+(add-hook 'post-command-hook 'show-fly-err-at-point)
+;; Set as a minor mode for Python
+(add-hook 'python-mode-hook '(lambda () (flymake-mode)))
+
 
 
 ;;settings for R
