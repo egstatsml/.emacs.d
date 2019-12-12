4ca0f6c26a9185dd7875a4f98e60dfee677bff4b
Ethan <ethanjgoan@gmail.com>
2019-12-12 14:37:36 +1000

    matlab init file

---
+++ matlab_init.el
@@ -0,0 +1,9 @@
+;; ;; settings for MATLAB
+;; (add-to-list 'load-path "~/.emacs.d/packages/matlab/matlab.el")
+;; (load-library "matlab-load")
+;; (add-hook 'matlab-mode-hook 'company-mode)
+;; (add-hook 'matlab-shell-mode-hook 'company-mode)
+;; (setq auto-mode-alist
+;;    (cons
+;;     '("\\.m$" . matlab-mode)
+;;     auto-mode-alist))
