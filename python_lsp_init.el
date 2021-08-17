c2e6e5477856ef3f723a64794a7555a93d9e23a7
Ethan Goan <ethanjgoan@gmail.com>
2021-08-17 14:08:11 +1000

    python server with lsp-mode init

---
+++ python_lsp_init.el
@@ -0,0 +1,6 @@
+(use-package lsp-python-ms
+  :ensure t
+  :init (setq lsp-python-ms-auto-install-server t)
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-python-ms)
+                          (lsp))))  ; or lsp-deferred
