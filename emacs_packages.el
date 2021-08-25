e22b28889e90623818dca8576a7a8baf11311f1c
Ethan Goan <ethanjgoan@gmail.com>
2021-08-25 15:04:34 +1000

    updated package list

--- emacs_packages.el
+++ emacs_packages.el
@@ -1,5 +1,4 @@
-
-;;Istalled packages and script to install them
+;;Installed packages and script to install them
 ;;
 ;;Can get a list of installed packages from Emacs
 ;;by running C-h v package-activated-list
@@ -33,118 +32,137 @@
     (package-install package)))
 
 ;; list of packages to install
-(setq pkgs '(auctex
-org-journal
-ac-html
-all-the-icons-ivy-rich
-all-the-icons
-apropospriate-theme
-auctex-latexmk
-auctex
-auto-complete-auctex
-auto-complete
-calfw
-color-theme
-color-theme-sanityinc-solarized
-color-theme-sanityinc-tomorrow
-counsel
-ebib
-ein
-deferred
-anaphora
-elpy
-company
-ess-R-data-view
-ctable
-ess-smart-equals
-ess-smart-underscore
-ess-view
-ess
-excorporate
-flycheck
-forge
-closql
-fsm
-ghub
-helm-ls-git
-helm-org
-highlight-indentation
-ivy-bibtex
-ivy-prescient
-ivy-rich
-jupyter
-langtool
-languagetool
-lsp-python-ms
-lsp-mode
-dash-functional
-magit
-git-commit
-markdown-mode
-matlab-mode
-mu4e-alert
-alert
-log4e
-gntp
-org-bullets
-org-kanban
-org-roam-bibtex
-org-ref
-key-chord
-hydra
-lv
-helm-bibtex
-helm
-htmlize
-bibtex-completion
-biblio
-biblio-core
-org-roam
-magit-section
-emacsql-sqlite
-emacsql
-f
-org-super-agenda
-ht
-org-wiki
-helm-core
-async
-parsebib
-pdf-view-restore
-pdf-tools
-pkg-info
-epl
-polymode
-popup
-prescient
-pyvenv
-request
-simple-httpd
-soap-client
-spinner
-stan-mode
-swiper
-ivy
-tablist
-transient
-treepy
-ts
-s
-dash
-url-http-ntlm
-use-package
-bind-key
-wakatime-mode
-web-completion-data
-websocket
-wgrep
-with-editor
-writegood-mode
-yaml
-yasnippet
-zenburn-theme
-zmq)
-) ;; (you probably want to format the packages.txt first, otherwise you get a huge single line)
+(setq pkgs
+      '(ac-html
+        all-the-icons-ivy
+        all-the-icons-ivy-rich
+        apropospriate-theme
+        auctex-latexmk
+        auctex
+        auto-complete-auctex
+        auto-complete
+        calfw
+        color-theme
+        color-theme-sanityinc-solarized
+        color-theme-sanityinc-tomorrow
+        counsel-projectile
+        counsel
+        dap-mode
+        bui
+        doom-modeline
+        doom-themes
+        all-the-icons
+        ebib
+        ein
+        deferred
+        anaphora
+        elpy
+        company
+        ess-R-data-view
+        ctable
+        ess-smart-equals
+        ess-smart-underscore
+        ess-view
+        ess
+        excorporate
+        flycheck
+        forge
+        closql
+        fsm
+        ghub
+        helm-ls-git
+        helm-org
+        helpful
+        elisp-refs
+        highlight-indentation
+        ivy-bibtex
+        ivy-prescient
+        ivy-rich
+        jupyter
+        langtool
+        languagetool
+        loop
+        lsp-ivy
+        lsp-python-ms
+        lsp-treemacs
+        lsp-mode
+        dash-functional
+        magit
+        git-commit
+        markdown-mode
+        matlab-mode
+        mu4e-alert
+        alert
+        log4e
+        gntp
+        org-bullets
+        org-journal
+        org-kanban
+        org-roam-bibtex
+        org-ref
+        key-chord
+        helm-bibtex
+        helm
+        htmlize
+        bibtex-completion
+        biblio
+        biblio-core
+        org-roam
+        magit-section
+        emacsql-sqlite
+        emacsql
+        org-super-agenda
+        org-wiki
+        helm-core
+        async
+        parsebib
+        pdf-view-restore
+        pdf-tools
+        pkg-info
+        epl
+        polymode
+        popup
+        posframe
+        prescient
+        projectile
+        pyvenv
+        rainbow-delimiters
+        request
+        shrink-path
+        simple-httpd
+        soap-client
+        spinner
+        stan-mode
+        swiper
+        ivy
+        tablist
+        transient
+        treemacs
+        ht
+        hydra
+        lv
+        pfuture
+        ace-window
+        avy
+        f
+        treepy
+        ts
+        s
+        dash
+        url-http-ntlm
+        use-package
+        bind-key
+        wakatime-mode
+        web-completion-data
+        websocket
+        wgrep
+        which-key
+        with-editor
+        writegood-mode
+        yaml
+        yasnippet
+        zenburn-theme
+        zmq)) ;; (you probably want to format the packages.txt first, otherwise you get a huge single line)
 (require 'cl) ;;g Common Lisp compatibility library
 (loop for pkg in pkgs do
   (require-package pkg)) ;; install each package
