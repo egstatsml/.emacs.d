bd1ec16318690f0c53184cedd0e7f68567a7be22
Ethan Goan <ethanjgoan@gmail.com>
2021-09-13 16:22:06 +1000

    updated list of emacs packages I use

--- emacs_packages.el
+++ emacs_packages.el
@@ -20,7 +20,7 @@
       '(("melpa-stable" . 20)
         ("org" . 20)
         ("gnu" . 10)
-        ("melpa" . 0)))
+        ("melpa" . 100)))
 
 
 ;; function that will install all the packages
@@ -33,136 +33,141 @@
 
 ;; list of packages to install
 (setq pkgs
-      '(ac-html
-        all-the-icons-ivy
-        all-the-icons-ivy-rich
-        apropospriate-theme
-        auctex-latexmk
-        auctex
-        auto-complete-auctex
-        auto-complete
-        calfw
-        color-theme
-        color-theme-sanityinc-solarized
-        color-theme-sanityinc-tomorrow
-        counsel-projectile
-        counsel
-        dap-mode
-        bui
-        doom-modeline
-        doom-themes
-        all-the-icons
-        ebib
-        ein
-        deferred
-        anaphora
-        elpy
-        company
-        ess-R-data-view
-        ctable
-        ess-smart-equals
-        ess-smart-underscore
-        ess-view
-        ess
-        excorporate
-        flycheck
-        forge
-        closql
-        fsm
-        ghub
-        helm-ls-git
-        helm-org
-        helpful
-        elisp-refs
-        highlight-indentation
-        ivy-bibtex
-        ivy-prescient
-        ivy-rich
-        jupyter
-        langtool
-        languagetool
-        loop
-        lsp-ivy
-        lsp-python-ms
-        lsp-treemacs
-        lsp-mode
-        dash-functional
-        magit
-        git-commit
-        markdown-mode
-        matlab-mode
-        mu4e-alert
-        alert
-        log4e
-        gntp
-        org-bullets
-        org-journal
-        org-kanban
-        org-roam-bibtex
-        org-ref
-        key-chord
-        helm-bibtex
-        helm
-        htmlize
-        bibtex-completion
-        biblio
-        biblio-core
-        org-roam
-        magit-section
-        emacsql-sqlite
-        emacsql
-        org-super-agenda
-        org-wiki
-        helm-core
-        async
-        parsebib
-        pdf-view-restore
-        pdf-tools
-        pkg-info
-        epl
-        polymode
-        popup
-        posframe
-        prescient
-        projectile
-        pyvenv
-        rainbow-delimiters
-        request
-        shrink-path
-        simple-httpd
-        soap-client
-        spinner
-        stan-mode
-        swiper
-        ivy
-        tablist
-        transient
-        treemacs
-        ht
-        hydra
-        lv
-        pfuture
-        ace-window
-        avy
-        f
-        treepy
-        ts
-        s
-        dash
-        url-http-ntlm
-        use-package
-        bind-key
-        wakatime-mode
-        web-completion-data
-        websocket
-        wgrep
-        which-key
-        with-editor
-        writegood-mode
-        yaml
-        yasnippet
-        zenburn-theme
-        zmq)) ;; (you probably want to format the packages.txt first, otherwise you get a huge single line)
+      '(0blayout
+        ac-html
+all-the-icons-ivy
+all-the-icons-ivy-rich
+apropospriate-theme
+auctex-latexmk
+auctex
+auto-complete-auctex
+auto-complete
+calfw
+color-theme
+color-theme-sanityinc-solarized
+color-theme-sanityinc-tomorrow
+counsel-projectile
+counsel
+dap-mode
+bui
+doom-modeline
+doom-themes
+all-the-icons
+ebib
+ein
+deferred
+anaphora
+elpy
+company
+ess-R-data-view
+ctable
+ess-smart-equals
+ess-smart-underscore
+ess-view
+ess
+evil-collection
+annalist
+evil
+excorporate
+flycheck
+forge
+closql
+fsm
+ghub
+goto-chg
+helm-ls-git
+helm-org
+helpful
+elisp-refs
+highlight-indentation
+ivy-bibtex
+ivy-prescient
+ivy-rich
+jupyter
+langtool
+languagetool
+loop
+lsp-ivy
+lsp-python-ms
+lsp-treemacs
+lsp-mode
+dash-functional
+magit
+git-commit
+markdown-mode
+matlab-mode
+mu4e-alert
+alert
+log4e
+gntp
+org-bullets
+org-journal
+org-kanban
+org-noter
+org-roam-bibtex
+org-ref
+key-chord
+helm-bibtex
+helm
+htmlize
+bibtex-completion
+biblio
+biblio-core
+org-roam
+magit-section
+emacsql-sqlite
+emacsql
+org-super-agenda
+helm-core
+parsebib
+pdf-view-restore
+pdf-tools
+pkg-info
+epl
+polymode
+popup
+posframe
+prescient
+projectile
+pyvenv
+rainbow-delimiters
+request
+shrink-path
+simple-httpd
+soap-client
+spinner
+stan-mode
+swiper
+ivy
+tablist
+transient
+treemacs
+ht
+hydra
+lv
+pfuture
+ace-window
+avy
+f
+treepy
+ts
+s
+dash
+undo-tree
+url-http-ntlm
+use-package
+bind-key
+wakatime-mode
+web-completion-data
+websocket
+wgrep
+which-key
+with-editor
+writegood-mode
+yaml
+yasnippet
+zenburn-theme
+zmq)) ;; (you probably want to format the packages.txt first, otherwise you get a huge single line)
 (require 'cl) ;;g Common Lisp compatibility library
 (loop for pkg in pkgs do
   (require-package pkg)) ;; install each package
