60a0b1909eefd56d7c7121416d3df2d08bc94b00
Ethan Goan <ethanjgoan@gmail.com>
2021-10-05 00:25:40 +0000

    added send end character support for vterm. use zsh-vim-mode

--- init.el
+++ init.el
@@ -130,13 +130,18 @@
   :config
   (setq which-key-idle-delay 1))
 
-;; I like using vterm
+
+(defun vterm-set-evil-escape ()
+  (local-set-key (kbd "C-[") 'vterm-send-escape))
+  ;; I like using vterm
 (use-package vterm
   :commands vterm
+  ;; :hook (vterm-set-evil-escape)
+  :bind ("<escape>" . vterm-send-escape)
   :config
   (setq vterm-timer-delay 0.01)
   (setq vterm-max-scrollback 10000))
-;; turn off linum mode for terminals
+  ;;turn off linum mode for terminals
 (defun nolinum ()
   (linum-mode 0))
 (add-hook 'term-mode-hook 'nolinum)
