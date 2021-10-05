1a0de56be1c63f24d58f798bb1ae1b83537043fc
Ethan Goan <ethanjgoan@gmail.com>
2021-10-05 10:35:32 +0000

    added markers to mu4e

--- init_mu4e.el
+++ init_mu4e.el
@@ -4,3 +4,8 @@
 
 ;; allow for updating mail using 'U' in the main view:
 (setq mu4e-get-mail-command "offlineimap")
+
+;; adding mu4e markers
+(use-package mu4e-marker-icons
+  :ensure t
+  :init (mu4e-marker-icons-mode 1))
