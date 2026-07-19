025d1bbdbc1933dd067eb8a72455102cc9547ed7
Ethan Goan <ethanjgoan@gmail.com>
2026-07-19 19:57:21 +1000

    feat: undo-fu and undo-fu-sessions

--- user-lisp/setup-vundo.el
+++ user-lisp/setup-vundo.el
@@ -1,6 +1,25 @@
 ;;; setup-vundo.el -*- lexical-binding: t; no-byte-compile: t; -*-
 
 
+(use-package undo-fu
+  :ensure t
+  :init
+  ;; increasing undo limits
+  (setq undo-limit 67108864) ; 64mb.
+  (setq undo-strong-limit 100663296) ; 96mb.
+  (setq undo-outer-limit 1006632960) ; 960mb.
+  :config
+  (global-unset-key (kbd "C-z"))
+  (global-set-key (kbd "C-z")   'undo-fu-only-undo)
+  (global-set-key (kbd "C-S-z") 'undo-fu-only-redo))
+
+(use-package undo-fu-session
+  :ensure t
+  :config
+  (setq undo-fu-session-incompatible-files '("/COMMIT_EDITMSG\\'" "/git-rebase-todo\\'"))
+  (undo-fu-session-global-mode))
+
+
 (use-package vundo
   :ensure (vundo
 	   :host github
@@ -9,8 +28,8 @@
   ("C-x u" . vundo)
   :config
   (setq vundo-glyph-alist vundo-unicode-symbols
-	vundo-compact-display t)
+	vundo-compact-display t))
   ;; this is to ensure that the tree is all aligned in vundo
   ;; just explicitly forcing it to use a monospace font here.
-  (set-face-attribute 'vundo-default nil :font "Iosevka Nerd Font Mono" :family "Iosevka Nerd Font"))
+  ;; (set-face-attribute 'vundo-default nil :font "JetBrainsMono Nerd Font Mono") :family "Iosevka Nerd Font"))
 (provide 'setup-vundo)
